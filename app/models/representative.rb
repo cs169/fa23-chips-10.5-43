9b089ae8bc2f62b614101d34d730051ffa655c41
Basem Moustafa <bmoustafa@berkeley.edu>
2023-11-28 05:18:36 +0000

    1.2 added more info to representative

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -9,6 +9,13 @@ class Representative < ApplicationRecord
     rep_info.officials.each_with_index do |official, index|
       ocdid_temp = ''
       title_temp = ''
+      url =  official.photo_url || 'https://picsum.photos/id/378/200/300'
+      if official.address
+        street = official.address[0].line1 || ''
+        city = official.address[0].city || ''
+        state = official.address[0].state || ''
+        zip = official.address[0].zip || ''
+      end
 
       rep_info.offices.each do |office|
         if office.official_indices.include? index
@@ -18,7 +25,7 @@ class Representative < ApplicationRecord
       end
 
       rep = Representative.find_or_create_by!({ name: official.name, ocdid: ocdid_temp,
-          title: title_temp })
+          title: title_temp, party: official.party, address_street: street, address_city: city, address_state: state, address_zip: zip, photo_url: url})
       reps.push(rep)
     end
 
